515d706934e261e1ff10259e22747187b7a936e4
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-18 19:05:52 -0700

    Completely finished lab 3's problem 2

--- turnin/fgonz059_lab3_part2.c
+++ turnin/fgonz059_lab3_part2.c
@@ -24,12 +24,12 @@ int main(void) {
 	while (1) {
 		tempA = PINA & 0x0F;
 		tempC = 0x00;
-		if (      (tempA == 1) || (tempA == 2) { tempC = tempC | 0x20; /* PC5 */ }
-		else if ( (tempA == 3) || (tempA == 4) { tempC = tempC | 0x30;/* PC5 PC4 */ }
-		else if ( (tempA == 5) || (tempA == 6) { tempC = tempC | 0x38;/* PC5 PC4 PC3 */ }
-		else if ( (tempA >= 7) && (tempA <= 9) { tempC = tempC | 0x3C;/* PC5 PC4 PC3 PC2 */ }
-		else if ( (tempA >= 10) &&(tempA <=12) { tempC = tempC | 0x3E;/* PC5 PC4 PC3 PC2 PC1 */ }
-		else if ( (tempA >= 13) &&(tempA <=15) { tempC = tempC | 0x3F;/* PC5 PC4 PC3 PC2 PC1 PC0 */ }
+		if (      (tempA == 1) || (tempA == 2)) { tempC = tempC | 0x20; /* PC5 */ }
+		else if ( (tempA == 3) || (tempA == 4)) { tempC = tempC | 0x30;/* PC5 PC4 */ }
+		else if ( (tempA == 5) || (tempA == 6)) { tempC = tempC | 0x38;/* PC5 PC4 PC3 */ }
+		else if ( (tempA >= 7) && (tempA <= 9)) { tempC = tempC | 0x3C;/* PC5 PC4 PC3 PC2 */ }
+		else if ( (tempA >= 10) &&(tempA <=12)) { tempC = tempC | 0x3E;/* PC5 PC4 PC3 PC2 PC1 */ }
+		else if ( (tempA >= 13) &&(tempA <=15)) { tempC = tempC | 0x3F;/* PC5 PC4 PC3 PC2 PC1 PC0 */ }
 		
 		if ( tempA <= 4 ) { tempC = tempC | 0x40; /* PC6 */ } // Low fuel
 		
