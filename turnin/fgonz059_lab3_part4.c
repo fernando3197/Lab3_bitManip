72ac1c82e52789215aa3c98723f3a4d61cca2895
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-18 21:53:20 -0700

    4th try

--- turnin/fgonz059_lab3_part4.c
+++ turnin/fgonz059_lab3_part4.c
@@ -16,16 +16,20 @@ int main(void) {
     
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
-
+	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tempA = 0x00;
 	unsigned char tempB = 0x00;
-	
+	unsigned char tempC = 0x00;
 	while (1) {
 		tempA = PINA;
 		tempB = 0x00;
+		tempC = 0x00;
 		
-		tempB = (tempB | ((tempA & 0x0F) << 4 )) | ((tempA & 0xF0) >> 4);
+		tempB = tempB | ((tempA & 0xF0 ) >> 4);
+		tempC = tempB | ((tempA & 0x0F) << 4 );
+		
 		PORTB = tempB;
+		PORTC = tempC;
 	}
     return 1;
 }
