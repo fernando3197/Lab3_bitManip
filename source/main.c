558e6302e68b3b5aa8185e3c4ab5823ef1bcf0d1
Fernando Gonzalez <fgonz059@wch136-24.cs.ucr.edu>
2020-10-18 18:15:29 -0700

    adsf

--- source/main.c
+++ source/main.c
@@ -15,27 +15,14 @@
 int main(void) {
     
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 
 	unsigned char tempA = 0x00;
-	unsigned char tempB = 0x00;
+	unsigned char tempC = 0x00;
 	unsigned char count = 0x00;
 	unsigned char i = 0;
 	while (1) {
-		tempA = PINA;
-		tempB = PINB;
-		count = 0;
-		for (i = 0; i < 8; i = i + 1){
-			if (tempA & (0x01 << i ) ){
-				count = count + 1;	
-			}
-
-			if (tempB & (0x01 << i ) ){
-				count = count + 1;
-			}
-		}
-		PORTC = count;		
+				
 	}
     return 1;
 }
